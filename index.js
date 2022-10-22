c60f8263595eb8455fbf3694988dca1bc6c44eb4
Tiffany Cheng <tiffanycheng.001@gmail.com>
2022-10-21 19:37:20 -0600

    commit tree 43889adad4656ae9a392cbef1258ef251249922c

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(monday = 'go to the office'){
+    return `This Monday, I will ${monday}.`
+}
